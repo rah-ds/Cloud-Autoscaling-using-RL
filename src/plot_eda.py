f63e3067036c3b64c4fd4d21df30b402b0dbc473
Ryan Healy <>
2025-10-23 11:18:26 -0400

    adding in py files

---
+++ src/plot_eda.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Set Seaborn style
+sns.set(style="whitegrid")
+plt.rcParams['figure.figsize'] = (12, 6)
+
+# load the Borg traces dataset
+df_borg = pd.read_csv("../data/raw/borg_traces.zip")
+print(df_borg.shape)
+# df_borg.head().T
+
+
+# plot by average_cpu_usage over time
+plt.figure(figsize=(14, 7))
+sns.lineplot(data=df_borg, x='time_sec', y='average_cpu_usage', ci=None)
+plt.title('Average CPU Usage Over Time')
+plt.xlabel('Time (seconds)')
+plt.ylabel('Average CPU Usage')
+plt.show()
