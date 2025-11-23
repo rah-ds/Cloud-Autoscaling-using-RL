084ed3078b1fe5aa17b4672926fb600b3c23914f
Bruce McGregor22 <bruce.mcgregor22@gmail.com>
2025-11-23 18:34:15 -0500

    Added DoubleQNetwork Agent

---
+++ scripts/Double_DQN_Agent.py
@@ -0,0 +1,72 @@
+
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import random
+from collections import deque
+import pandas as pd
+from DQN_Utils import QNetwork
+
+
+class DoubleDQNAgent:
+    """Interacts with and learns from the environment using Double DQN."""
+
+    def __init__(self, observation_space_shape, action_space_size, seed, buffer_size=int(1e5), batch_size=64, gamma=0.99, lr=5e-4, tau=1e-3, update_every=4):
+        self.observation_space_shape = observation_space_shape
+        self.action_space_size = action_space_size
+        self.seed = random.seed(seed)
+        self.gamma = gamma
+        self.tau = tau
+        self.update_every = update_every
+
+        self.qnetwork_local = QNetwork(observation_space_shape, action_space_size).to(device)
+        self.qnetwork_target = QNetwork(observation_space_shape, action_space_size).to(device)
+        self.optimizer = optim.Adam(self.qnetwork_local.parameters(), lr=lr)
+
+        self.memory = ReplayBuffer(buffer_size, batch_size)
+
+        self.t_step = 0
+
+    def step(self, state, action, reward, next_state, done):
+        self.memory.add(state, action, reward, next_state, done)
+
+        self.t_step = (self.t_step + 1) % self.update_every
+        if self.t_step == 0:
+            if len(self.memory) > self.memory.batch_size:
+                experiences = self.memory.sample()
+                self.learn(experiences, self.gamma)
+
+    def select_action(self, state, eps=0.):
+        state = torch.from_numpy(state).float().unsqueeze(0).to(device)
+        self.qnetwork_local.eval()
+        with torch.no_grad():
+            action_values = self.qnetwork_local(state)
+        self.qnetwork_local.train()
+
+        if random.random() > eps:
+            return np.argmax(action_values.cpu().data.numpy())
+        else:
+            return random.choice(np.arange(self.action_space_size))
+
+    def learn(self, experiences, gamma):
+        states, actions, rewards, next_states, dones = experiences
+
+        actions_next = self.qnetwork_local(next_states).detach().argmax(1).unsqueeze(1)
+        Q_targets_next = self.qnetwork_target(next_states).gather(1, actions_next)
+
+        Q_targets = rewards + (gamma * Q_targets_next * (1 - dones))
+
+        Q_expecteds = self.qnetwork_local(states).gather(1, actions)
+
+        loss = nn.MSELoss()(Q_expecteds, Q_targets)
+
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+
+        self.soft_update(self.qnetwork_local, self.qnetwork_target, self.tau)
+
+    def soft_update(self, local_model, target_model, tau):
+        for target_param, local_param in zip(target_model.parameters(), local_model.parameters()):
+            target_param.data.copy_(tau*local_param.data + (1.0-tau)*target_param.data)
