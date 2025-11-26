fcdac5fbc70393bec22da431df2158f5c4938a9d
Bruce McGregor22 <bruce.mcgregor22@gmail.com>
2025-11-25 21:06:09 -0500

    corrected DQNAgent.py

--- scripts/DQN_Agent.py
+++ scripts/DQN_Agent.py
@@ -22,7 +22,7 @@ class DQNAgent:
         self.qnetwork_local = QNetwork(observation_space_shape, action_space_size).to(self.device)
         self.qnetwork_target = QNetwork(observation_space_shape, action_space_size).to(self.device)
         self.optimizer = optim.Adam(self.qnetwork_local.parameters(), lr=lr)
-        self.memory = ReplayBuffer(buffer_size=buffer_size, batch_size=batch_size, devic=self.device,seed=seed)
+        self.memory = ReplayBuffer(buffer_size=buffer_size, batch_size=batch_size, device=self.device,seed=seed)
         self.t_step = 0
         
 
